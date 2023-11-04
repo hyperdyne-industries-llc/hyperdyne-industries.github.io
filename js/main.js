2551098ce5528daabd59749de7a0f1216f02fab1
kenreilly <kenneth.reilly@hotmail.com>
2023-11-04 11:38:19 -0600

    Completed basic design and content for widescreen and mobile

--- js/main.js
+++ js/main.js
@@ -1,2 +1,4 @@
-let calc_scroll = () => Math.floor((3.75 * (0.25 + Math.floor(window.scrollY) / Math.floor(document.body.scrollHeight - window.innerHeight))) - 0.75)
+// let calc_scroll = () => Math.floor((3.75 * (0.25 + Math.floor(window.scrollY) / Math.floor(document.body.scrollHeight - window.innerHeight))) - 0.75)
+
+let calc_scroll = () => Math.floor((4 * (0.25 + Math.floor(window.scrollY) / Math.floor(document.body.scrollHeight - window.innerHeight))) - 0.25)
 document.onscroll = (ev) => { document.body.dataset.scroll = calc_scroll() }
